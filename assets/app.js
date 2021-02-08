e7e40ec2b6f37115b49e3ed23fe01297ef9cd1d6
rositatisor <70884246+rositatisor@users.noreply.github.com>
2021-02-08 16:32:00 +0200

    webpack, bootstrap, summernote, jquery, popper.js

---
+++ assets/app.js
@@ -0,0 +1,29 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+// import './bootstrap';
+
+import 'bootstrap';
+
+const $ = require('jquery');
+const summernote = require('summernote');
+
+$('#summernote').summernote({
+    toolbar: [
+        ['style', ['bold', 'italic', 'underline', 'clear']],
+        ['font', ['strikethrough', 'superscript', 'subscript']],
+        ['fontsize', ['fontsize']],
+        ['color', ['color']],
+        ['height', ['height']]
+    ]
+});
+console.log(summernote);
+console.log('labas');
